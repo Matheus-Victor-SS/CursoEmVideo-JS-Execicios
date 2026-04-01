759fe863faffb5b502b8537319353893e01a2718
Matheus <matheus.victor.ss48@gmail.com>
2026-04-01 10:58:19 -0300

    novo exercicio testando as funcionalidades de vetores no terminal node

---
+++ Exercícios Simples/11 - Array/11.1 - Array.js
@@ -0,0 +1,20 @@
+var num = [5, 6, 8, 2, 1, 6, 7, 9]
+num.push(10)
+num.sort()
+console.log(num)
+console.log(`Dentro do vetor existem ${num.length} valores`)
+console.log(`O primeiro valor do vetor é ${num[0]}<br>`)
+/*
+var pos = 0
+while(pos<num.length){
+    console.log(`a posição ${pos} tem o valor: ${num[pos]}`)
+    pos++
+}*/
+
+//versao mais simples(para cada posição em num, faça:)
+var a = num.indexOf(6)
+for(let pos in num){
+    console.log(`a posição ${pos} tem o valor: ${num[pos]}`)
+    pos++
+}
+console.log(a)
