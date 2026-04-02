44b6a8c98947da24bde298575aed31e426bac147
Matheus <matheus.victor.ss48@gmail.com>
2026-04-02 09:48:42 -0300

    comentarios no exercicio de array

--- Exercícios Simples/11 - Array/11.1 - Array.js
+++ Exercícios Simples/11 - Array/11.1 - Array.js
@@ -1,5 +1,6 @@
 var num = [5, 6, 8, 2, 1, 6, 7, 9]
 num.push(10)
+//poe em ordem
 num.sort()
 console.log(num)
 console.log(`Dentro do vetor existem ${num.length} valores`)
@@ -11,8 +12,9 @@ while(pos<num.length){
     pos++
 }*/
 
-//versao mais simples(para cada posição em num, faça:)
+//busca o valor 6 e devolve seu indice
 var a = num.indexOf(6)
+//versao mais simples(para cada posição em num, faça:)
 for(let pos in num){
     console.log(`a posição ${pos} tem o valor: ${num[pos]}`)
     pos++
